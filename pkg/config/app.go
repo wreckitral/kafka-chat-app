adf188065fc0447120349fe5048d5657dcbb3488
wreckitral <defhanayasofhiea@gmail.com>
2025-01-28 11:56:22 +0700

    feat(config): add application spesific config

---
+++ pkg/config/app.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	"os"
+	"strconv"
+	"time"
+)
+
+type App struct {
+    Host        string
+    Port        int
+    Debug       bool
+    ReadTimeout time.Duration
+
+    JWTSecretKey                string
+    JWTSecretExpireMinutesCount int
+}
+
+var app = &App{}
+
+func AppCfg() *App {
+    return app
+}
+
+func LoadApp() {
+    app.Host = os.Getenv("APP_HOST")
+    app.Port, _ = strconv.Atoi(os.Getenv("APP_PORT"))
+    app.Debug, _ = strconv.ParseBool(os.Getenv("APP_ DEBUG"))
+    timeOut, _ := strconv.Atoi(os.Getenv("APP_READ_TIMEOUT"))
+    app.ReadTimeout = time.Duration(timeOut) * time.Second
+
+    app.JWTSecretKey = os.Getenv("APP_PORT")
+    app.JWTSecretExpireMinutesCount, _ = strconv.Atoi(os.Getenv("JWT_SECRET_KEY_EXPIRE_MINUTES_COUNT"))
+}
